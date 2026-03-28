ecfb9c9c9d5364e9932eb0e6cedef07bc7939a54
Kontra_N356 <naufalnabillansyah49@gmail.com>
2026-03-28 14:25:52 +0700

    second commit

---
+++ KodePaket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class KodePaket
+{
+
+    private static Dictionary<string, string> kodePaket = new Dictionary<string, string>{
+        { "Basic", "P201"},
+        {"Standard", "P202"},
+        {"Premium", "P203"},
+        {"Unlimited", "P204"},
+        {"Gaming", "P205"},
+        {"Streaming", "P206"},
+        {"Family", "P207"},
+        {"Business", "P208"},
+        {"Student", "P209"},
+        {"Traveler", "P210" }
+    };
+
+    public static string getKodePaket(string namaPaket)
+    {
+        string kodeProduk = "";
+        kodePaket.TryGetValue(namaPaket, out kodeProduk);
+        return kodeProduk;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("####Soal 1");
+        Console.Write("Masukkan Nama Paket: ");
+        string inNamaPaket = Console.ReadLine();
+        string gettedKodePaket = KodePaket.getKodePaket(inNamaPaket);
+        if (gettedKodePaket.Length <= 0)
+        {
+            Console.WriteLine("Nama Paket tidak ditemukan");
+        }
+        else
+        {
+            Console.WriteLine("Kode Paket: " + gettedKodePaket);
+        }
+    }
+}
